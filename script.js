bfe922b77011e9c94a23cc1e51f66c2ce9829d93
Veronfc <veronfc1701@gmail.com>
2023-08-07 08:34:15 +0200

    update project

--- script.js
+++ script.js
@@ -1,21 +1,56 @@
-const btn = document.querySelector('generate');
-btn.addEventListener('click', generate);
+const genbtn = document.getElementById('generate');
+genbtn.addEventListener('click', generate);
 
+const clearbtn = document.getElementById('clear');
+clearbtn.addEventListener('click', clear)
 
-function grid(squares) {
+const grid = document.getElementById('grid');
+
+function draw(squares) {
   for (let i=0; i<(Math.pow(squares, 2)); i++) {
-    const grid = document.getElementById('grid');
 
     const block = document.createElement('div');
     block.classList.add('block');
     block.style.width = (650/squares) + 'px';
     block.style.height = (650/squares) + 'px';
+    block.onmouseenter = function() {
+      block.style.backgroundColor = 'rgb(' + color() + ',' + color() + ',' + color() + ')';
+    }
 
     grid.appendChild(block);
   }
+
+  color();
 }
 
 function generate() {
-  let squares = prompt('Enter an integer from 1 to 100:');
-  grid(squares);
+  grid.innerHTML = '';
+
+  let squares = Math.floor(prompt('Enter an integer from 1 to 100:'));
+
+  if (!Number.isInteger(squares)){
+    squares = 64;
+  }
+
+  if (squares > 100) {
+    squares = 100;
+  } 
+  else if (squares < 0){
+    squares = 1;
+  }
+
+  draw(squares);
+}
+
+function color() {
+  let base =  Math.floor(Math.random() * (256));
+  
+  return base;
+}
+
+function clear() {
+  const all = grid.childNodes;
+  for (let i = 0; i < all.length; i++) {
+    all[i].backgroundColor = 'white';
+  }
 }
